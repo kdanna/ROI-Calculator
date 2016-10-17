1a115ee659576833750fc3bb184c64c4721bb16e
kdanna <kayce.danna@gmail.com>
2016-10-17 16:56:41 -0600

    beginning to add controller and scope

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var app = angular.module("myApp", []);
 
-function myCtrl($scope){
 
+app.controller('mainController', function($scope) {
+	$scope.itemDescription = "";
+	$scope.addRevenue = addRevenue;
+	$scope.revDesc = [];
 
+	function addRevenue() {
+		console.log('made it to the addRev');
+   		if ($scope.itemDescription){
+   			$scope.revDesc.push(this.itemDescription);  
+      	}
+      	$scope.itemDescription="";
+      	console.log($scope.revDesc);
+     }
 
-
-
-	
-}
+ 	
+});
