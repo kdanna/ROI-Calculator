5c7f8e35e43364642d55702540c5c4395a611922
kdanna <kayce.danna@gmail.com>
2016-10-18 20:31:36 -0600

    capturing Rev form inputs into an object

--- app.js
+++ app.js
@@ -2,24 +2,39 @@ var app = angular.module("myApp", []);
 
 
 app.controller('mainCtrl', function($scope){
-	$scope.itemDescription = '';
-	$scope.revDescs = [];
-	$scope.oneTimeRevs = [];
-	$scope.monthlyRevs = [];
+	// $scope.itemDescription = '';
+	// $scope.revDescs = [];
+	// $scope.oneTimeRevs = [];
+	// $scope.monthlyRevs = [];
 
-	$scope.addRevenue = function(){
-   		if ($scope.itemDescription){
-   			$scope.revDescs.push(this.itemDescription);  
-      	}
-      	if ($scope.oneTimeRev){
-      		$scope.oneTimeRevs.push(this.oneTimeRev);
-      	}
-      	if ($scope.monthlyRev){
-      		$scope.monthlyRevs.push(this.monthlyRev);
-      	}
-      	$scope.itemDescription="";
-      	$scope.oneTimeRev="";
-      	$scope.monthlyRev="";
+	// $scope.addRevenue = function(){
+ //   		if ($scope.itemDescription){
+ //   			$scope.revDescs.push(this.itemDescription);  
+ //      	}
+ //      	if ($scope.oneTimeRev){
+ //      		$scope.oneTimeRevs.push(this.oneTimeRev);
+ //      	}
+ //      	if ($scope.monthlyRev){
+ //      		$scope.monthlyRevs.push(this.monthlyRev);
+ //      	}
+ //      	$scope.itemDescription="";
+ //      	$scope.oneTimeRev="";
+ //      	$scope.monthlyRev="";
+	// };
+
+	$scope.revenueList = {
+		itemDescription: [],
+		oneTimeRev: [],
+		monthlyRev: []
 	};
 
+	$scope.addRevenue = function(revenue){
+		$scope.revenueList.itemDescription.push(revenue.itemDescription);
+		$scope.revenueList.oneTimeRev.push(revenue.oneTimeRev);
+		$scope.revenueList.monthlyRev.push(revenue.monthlyRev);
+		$scope.revenue.itemDescription="";
+      	$scope.revenue.oneTimeRev="";
+      	$scope.revenue.monthlyRev="";	
+	};
+		
 });
